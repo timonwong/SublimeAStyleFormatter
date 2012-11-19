c3fc6fe26e5a85276c149f87257373b6fea7104f
Timon Wong <timon86.wang@gmail.com>
2012-11-19 09:59:24 +0800

    merge: refactor

--- AStyleFormat.py
+++ AStyleFormat.py
@@ -53,6 +53,64 @@ def get_setting(key, default=None):
     return get_setting_view(sublime.active_window().active_view(), key, default)
 
 
+# borrowed from GoSublime
+class MergeException(Exception):
+    pass
+
+
+def _merge_code(view, edit, code, formatted):
+    def ss(start, end):
+        return view.substr(sublime.Region(start, end))
+
+    dmp = diff_match_patch()
+    diffs = dmp.diff_main(code, formatted)
+    dmp.diff_cleanupEfficiency(diffs)
+    i = 0
+    dirty = False
+    for k, s in diffs:
+        l = len(s)
+        if k == 0:
+            # match
+            l = len(s)
+            if ss(i, i + l) != s:
+                raise MergeException('mismatch', dirty)
+            i += l
+        else:
+            dirty = True
+            if k > 0:
+                # insert
+                view.insert(edit, i, s)
+                i += l
+            else:
+                # delete
+                if ss(i, i + l) != s:
+                    raise MergeException('mismatch', dirty)
+                view.erase(edit, sublime.Region(i, i + l))
+    return dirty
+
+
+def merge_code(view, edit, code, formatted_code):
+    vs = view.settings()
+    ttts = vs.get("translate_tabs_to_spaces")
+    vs.set("translate_tabs_to_spaces", False)
+    if not code.strip():
+        return (False, '')
+
+    dirty = False
+    err = ''
+    try:
+        dirty = _merge_code(view, edit, code, formatted_code)
+    except MergeException as (err, d):
+        dirty = True
+        err = "Could not merge changes into the buffer, edit aborted: %s" % err
+        view.replace(edit, sublime.Region(0, view.size()), code)
+    except Exception as ex:
+        err = "Unknown exception: %s" % ex
+    finally:
+        vs.set("translate_tabs_to_spaces", ttts)
+        return (dirty, err)
+
+
 class AstyleformatCommand(sublime_plugin.TextCommand):
     def get_language(self):
         caret = self.view.sel()[0].a
@@ -202,64 +260,6 @@ class AstyleformatCommand(sublime_plugin.TextCommand):
         # Add regions of formatted text
         [view.sel().add(region) for region in regions]
 
-    class MergeException(Exception):
-        pass
-
-    def _merge_code(self, edit, code, formatted):
-        # borrowed from GoSublime
-        view = self.view
-
-        def ss(start, end):
-            return view.substr(sublime.Region(start, end))
-
-        dmp = diff_match_patch()
-        diffs = dmp.diff_main(code, formatted)
-        dmp.diff_cleanupEfficiency(diffs)
-        i = 0
-        dirty = False
-        for k, s in diffs:
-            l = len(s)
-            if k == 0:
-                # match
-                l = len(s)
-                if ss(i, i + l) != s:
-                    raise self.MergeException('mismatch', dirty)
-                i += l
-            else:
-                dirty = True
-                if k > 0:
-                    # insert
-                    view.insert(edit, i, s)
-                    i += l
-                else:
-                    # delete
-                    if ss(i, i + l) != s:
-                        raise self.MergeException('mismatch', dirty)
-                    view.erase(edit, sublime.Region(i, i + l))
-        return dirty
-
-    def merge_code(self, edit, code, formatted_code):
-        view = self.view
-        vs = view.settings()
-        ttts = vs.get("translate_tabs_to_spaces")
-        vs.set("translate_tabs_to_spaces", False)
-        if not code.strip():
-            return (False, '')
-
-        try:
-            dirty = False
-            err = ''
-            dirty = self._merge_code(edit, code, formatted_code)
-        except self.MergeException as (err, d):
-            dirty = True
-            err = "Could not merge changes into the buffer, edit aborted: %s" % err
-            view.replace(edit, sublime.Region(0, view.size()), code)
-        except Exception as ex:
-            err = "where ma bees at?: %s" % ex
-        finally:
-            vs.set("translate_tabs_to_spaces", ttts)
-            return (dirty, err)
-
     def run_whole_file(self, edit, options):
         view = self.view
         region = sublime.Region(0, view.size())
@@ -267,7 +267,7 @@ class AstyleformatCommand(sublime_plugin.TextCommand):
         # Performing astyle formatter
         formatted_code = pyastyle.format(code, options)
         # Replace to view
-        _, err = self.merge_code(edit, code, formatted_code)
+        _, err = merge_code(view, edit, code, formatted_code)
         if err:
             sublime.error_message(
                 "SublimeAStyleFormatter: Merge failure: `%s'" % (
