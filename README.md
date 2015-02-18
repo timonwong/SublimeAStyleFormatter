e8d88744df39306d515debe587237e403187e8aa
Timon Wong <timon86.wang@gmail.com>
2015-02-18 21:56:32 +0800

    Upgrade pyastyle module and build script, for astyle v2.05.1

--- README.md
+++ README.md
@@ -149,7 +149,7 @@ License
 
 This plugin is using MIT License:
 
-    Copyright (c) 2012 Timon Wong
+    Copyright (c) 2012-2015 Timon Wong
 
     Permission is hereby granted, free of charge, to any person obtaining a copy of
     this software and associated documentation files (the "Software"), to deal in
--- src/user_build_py3.sh
+++ src/user_build_py3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -z "$PYTHON" ]; then
-	PYTHON=python
+if [[ -z "${PYTHON}" ]]; then
+	PYTHON=python3.3
 fi
 
 reset() {
@@ -10,36 +10,49 @@ reset() {
 
 reset
 
-CURRENT_PATH=`pwd`
+CURRENT_PATH=$(pwd)
 
-if [ $OSTYPE = "linux-gnu" ]; then
+if [[ "${OSTYPE}" = "linux-gnu" ]]; then
 	# In Linux, Sublime Text's Python is compiled with UCS4:
 	echo "Linux build!"
-	if [ `uname -m` == 'x86_64' ]; then
-		export CXXFLAGS="-fPIC $CFLAGS"
-		export CFLAGS="-fPIC $CFLAGS"
+	if [[ $(uname -m) == 'x86_64' ]]; then
+		pkg_folder="_linux_x86_64"
+
+		export CXXFLAGS="-fPIC ${CXXFLAGS}"
+		export CFLAGS="-fPIC ${CFLAGS}"
 	else
-		export CXXFLAGS="$CFLAGS"
-		export CFLAGS="$CFLAGS"
+		pkg_folder="_linux_x86"
+
+		export CXXFLAGS="${CFLAGS}"
+		export CFLAGS="${CFLAGS}"
 	fi
-elif [ ${OSTYPE:0:6} = "darwin" ]; then
+elif [[ "${OSTYPE:0:6}" = "darwin" ]]; then
 	echo "Mac OS X build!"
-	export ARCHFLAGS="-arch x86_64 $ARCHFLAGS"
-	export CXXFLAGS="-arch x86_64 $CFLAGS"
-	export CFLAGS="-arch x86_64 $CFLAGS"
-	export LDFLAGS="-arch x86_64 $LDFLAGS"
+	pkg_folder="_macosx_universal"
+	arch_flags="-arch x86_64"
+	osxver_flags="-stdlib=libstdc++ -mmacosx-version-min=10.7"
+
+	export ARCHFLAGS="${arch_flags} ${ARCHFLAGS}"
+	export CXXFLAGS="${arch_flags} ${osxver_flags} ${CXXFLAGS}"
+	export CFLAGS="${arch_flags} ${osxver_flags} ${CFLAGS}"
+	export LDFLAGS="${arch_flags} ${osxver_flags} ${LDFLAGS}"
+else
+	echo "Unknown system!"
+	exit 1
 fi
+
+target_folder="../pyastyle/python3/${pkg_folder}"
 
 (echo "Building pyastyle..." && \
 	cd pyastyle && \
-	$PYTHON setup.py build && \
-	cd "$CURRENT_PATH"
+	${PYTHON} setup.py build && \
+	cd "${CURRENT_PATH}"
 ) && \
-mkdir -p ../pyastyle/python3/_local_arch && \
-find . -type f -name "pyastyle*.so" -exec cp {} ../pyastyle/python3/_local_arch \; && \
+mkdir -p "${target_folder}" && \
+find . -type f -name "pyastyle.so" -exec cp {} "${target_folder}" \; && \
 
 reset && \
 echo "Done!" || \
-echo "Build Failed!$ERR"
+echo "Build Failed!${ERR}"
 
-strip ../pyastyle/python3/_local_arch/*.so > /dev/null 2>&1
+strip "${target_folder}/*.so" > /dev/null 2>&1
